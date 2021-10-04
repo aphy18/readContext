e4a0c59cf22b40dd45c03fe204ddfbc43f7d05f0
aphy18 <79496374+aphy18@users.noreply.github.com>
2021-10-04 19:43:45 -0400

    Initialize 2-login

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Info from 'pages/Info';
+import Login from 'pages/Login';
+import 'App.css';
+
+export default function App() {
+  const [auth, setAuth] = useState(false);
+  const [user, setUser] = useState(null);
+
+  // Perform some login process for the user
+  const login = function(email, password) {
+    setAuth(true);
+    setUser({ email, name: "Test user", id: "1234-5678-90ab" });
+  };
+
+  const logout = function() {
+    setAuth(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>My App</h1>
+      {!auth && <Login login={login} />}
+      {auth && <Info logout={logout} user={user} />}
+    </div>
+  );
+}
